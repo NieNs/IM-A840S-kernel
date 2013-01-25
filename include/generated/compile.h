92a26e14066b615017b3ebef6c6a053af225b036
NieNs <and_roid_@naver.com>
2013-01-25 14:20:11 +0900

    First commit+SIO

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Jan 24 19:51:51 KST 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "HeavenPup"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
